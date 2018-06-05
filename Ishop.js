1411112c2320b17af750ca785c5abb4660f5c8d7
Valera Mitar <miatr9108@gmail.com>
2018-06-06 02:11:16 +0300

    hirs step to create shop component

---
+++ Ishop.js
@@ -0,0 +1,33 @@
+var ItemComponent = React.createClass({
+
+    displayName: 'ItemInShop',
+  
+    propTypes: {
+        items:React.PropTypes.arrayOf(
+        React.PropTypes.shape({
+          itemCode: React.PropTypes.number.isRequired,
+          count: React.PropTypes.number.isRequired,
+          price: React.PropTypes.number.isRequired,
+          itemName: React.PropTypes.string.isRequired,
+          URLphoto: React.PropTypes.string.isRequired,
+          captionText: React.PropTypes.string.isRequired,
+        })
+      )
+    },
+  
+    render: function() {
+  
+      var itemsRow = this.props.items.map( el =>
+          React.DOM.div({key:el.itemCode,className:'itemsRow'},
+            React.DOM.span({className:'itemName'},el.itemName),
+            React.DOM.img({src:el.URLphoto, alt:el.captionText,className:'itemImage'}),
+            React.DOM.span({className:'itemPrice'},el.price),
+            React.DOM.span({className:'itemCount'},el.count),
+        ));
+
+      return React.DOM.div( {className:'shopItems'}, 
+        React.DOM.div( {className:'itemsContainer'}, itemsRow ),
+      );
+    },
+  
+  });
