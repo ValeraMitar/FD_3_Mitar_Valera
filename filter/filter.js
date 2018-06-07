b2dd18cf177e21e23eda537a70b219c5bab2743a
Valera Mitar <miatr9108@gmail.com>
2018-06-07 07:50:41 +0300

    some changes

--- filter/filter.js
+++ filter/filter.js
@@ -1,37 +1,67 @@
 var FilterComponent = React.createClass({
 
     displayName: 'Filter',
-    state={
-        isChecked = false,
+
+    getInitialState: function () {
+        return {
+            isChecked: false,
+            textArray: this.props.textList,
+            // textArrayOld: [],
+        };
     },
+
     propTypes: {
-        textList:React.PropTypes.arrayOf(
-        React.PropTypes.shape({
-            text: React.PropTypes.string.isRequired,
-        })
-      )
-    },
-  
-    onChecked: function() {
-        var inputState = this.state.isChecked;
-        this.setState( {isChecked:!inputState} );
+        // textList: React.PropTypes.string.isRequired,
+        filterLabel: React.PropTypes.string.isRequired,
     },
 
-    render: function() {
-  
-      var textListRow = this.props.textList.map( el =>
-          React.DOM.div({key:el.stringCode,className:'itemsRow'},
-            React.DOM.span({className:'listRow'},el.text),
-        ));
-
-      return React.DOM.div( {className:'filter'}, 
-        React.DOM.div( {className:'listContaoner'}, textListRow ),
-        React.DOM.input({
-            type:'checkbox',name:'filter',
-            onClick:this.onChecked,
-            checked: this.state.isChecked,
-        })
-      );
+    onChecked: function () {
+        var inputState = !this.state.isChecked;
+        var sortArray;
+        if (inputState) {
+            sortArray = this.state.textArray;
+            sortArray.sort();
+        } else {
+            sortArray = this.props.textList;
+        }
+        this.setState({ 
+            isChecked: inputState,
+            textArray: sortArray});
+        
     },
-  
-  });
+
+    handleChange: function (event) {
+        console.log(event.target.value);
+    },
+
+    render: function () {
+        var filteredArray = this.state.textArray;
+        var textListRow = filteredArray.map(el =>
+            React.DOM.div({ className: 'itemsRow' },
+                React.DOM.span({ className: 'listRow' }, el),
+            ));
+
+        return React.DOM.div({ className: 'filter' },
+            React.DOM.div({ className: 'checkBoxContainer' },
+                React.DOM.input({
+                    type: 'checkbox',
+                    name: 'filter',
+                    value: this.props.filterLabel,
+                    onClick: this.onChecked,
+                    defaultChecked: this.state.isChecked,
+                }),
+                React.DOM.span({ className: 'inputLabel' }, this.props.filterLabel)
+            ),
+            React.DOM.div({ className: 'inputSearchContainer' },
+                React.DOM.input({
+                    type: 'text',
+                    name: 'filter',
+                    onChange: this.handleChange,
+                    defaultChecked: 'Please enter filter marker',
+                }),
+            ),
+            React.DOM.div({ className: 'listContaoner' }, textListRow),
+        );
+    },
+
+});
